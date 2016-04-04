196b72c579328a556d042565937b499a624220e4
tibyte <ttl74283@gmail.com>
2016-04-05 01:48:26 +0900

    Wiring everything up

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -35,12 +35,24 @@ router.param('post', function(req, res, next, id) {
 
   query.exec(function (err, post){
     if (err) { return next(err); }
-    if (!post) { return next(new Error('can\'t find post')); }
+    if (!post) { return next(new Error("can't find post")); }
 
     req.post = post;
     return next();
   });
 });
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error("can't find comment")); }
+
+    req.comment = comment;
+    return next();
+  });
+});
+
 
 router.get('/posts/:post', function(req, res, next) {
   req.post.populate('comments', function(err, post) {
@@ -73,5 +85,14 @@ router.post('/posts/:post/comments', function(req, res, next) {
     });
   });
 });
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment){
+    if (err) { return next(err); }
+
+    res.json(comment);
+  });
+});
+
 
 module.exports = router;
